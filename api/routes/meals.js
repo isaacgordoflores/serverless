90699efb398bc56252017f4e329f5216e1ee9ea8
Isaac Gordo Flores <73773793+isaacgordoflores@users.noreply.github.com>
2020-11-01 19:22:14 +0100

    Actualizacion de los endpoints
    Actualizados ambos endpoints para:
    Obtener un listado, elemento.
    Crear, actualizar y eliminar elemento.

--- api/routes/meals.js
+++ api/routes/meals.js
@@ -1,11 +1,43 @@
 const express = require('express')
+const Meals = require('../models/Meals')
 
-const router = app.router()
+const router = express.Router()
+/**
+ * Para obtener un listado
+ */
 router.get('/', (req, res) => {
-    res.send('Hola GET Meals')
+    Meals.find()
+        .exec()
+        .then(x => res.status(200).send(x))
 })
+/**
+ * Para obtener un elemento
+ */
+router.get('/:id', (req, res) => {
+    Meals.findById(req.params.id)
+        .exec()
+        .then(x => res.status(200).send(x))
+})
+/**
+ * Para crear un elemento
+ */
 router.post('/', (req, res) => {
-    res.send('Hola POST Meals')
+    Meals.create(req.body).then(x => res.status(201).send(x))
+})
+/***
+ * Para actualizar un elemento
+ */
+router.put('/:id', (req, res) => {
+    Meals.findByIdAndUpdate(req.params.id, req.body)
+        .then(() => res.sendStatus(204))
+})
+/**
+ * Para eliminar un elemento
+ */
+router.delete('/:id', (req, res) => {
+    Meals.findByIdAndDelete(req.params.id)
+    .exec()
+    .then(() => res.sendStatus(204))
 })
 
 module.exports = router
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,11 +1,43 @@
 const express = require('express')
+const Orders = require('../models/Orders')
 
-const router = app.router()
+const router = express.Router()
+/**
+ * Para obtener un listado
+ */
 router.get('/', (req, res) => {
-    res.send('Hola GET Orders')
+    Orders.find()
+        .exec()
+        .then(x => res.status(200).send(x))
 })
+/**
+ * Para obtener un elemento
+ */
+router.get('/:id', (req, res) => {
+    Orders.findById(req.params.id)
+        .exec()
+        .then(x => res.status(200).send(x))
+})
+/**
+ * Para crear un elemento
+ */
 router.post('/', (req, res) => {
-    res.send('Hola POST Orders')
+    Orders.create(req.body).then(x => res.status(201).send(x))
+})
+/***
+ * Para actualizar un elemento
+ */
+router.put('/:id', (req, res) => {
+    Orders.findByIdAndUpdate(req.params.id, req.body)
+        .then(() => res.sendStatus(204))
+})
+/**
+ * Para eliminar un elemento
+ */
+router.delete('/:id', (req, res) => {
+    Orders.findByIdAndDelete(req.params.id)
+    .exec()
+    .then(() => res.sendStatus(204))
 })
 
 module.exports = router
