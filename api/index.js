57397dea4626c383aa35c0be3ebe0a09c2a6f12b
Isaac Gordo Flores <73773793+isaacgordoflores@users.noreply.github.com>
2020-11-01 16:57:11 +0100

    index.js

---
+++ api/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('bodyParser')   
+const cors = require('cors')
+
+const app = express()
+/**
+ * Para configurar buscar en la documentación de cada una
+ */
+app.use(bodyParser.json())
+app.use(cors())
+
+/**
+ * BDD CONN
+ * Conectar a traves de las variables de entorno (MONGO_URI) y le paso dos opciones de configuración 
+ * 
+ */
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+//Cuando llame a cualquier ruta con get, devuelvo string
+app.get('*',(req, res) => {
+    res.send({ mensaje: 'RESPUESTA PRUEBA' });
+})
+
+module.exports = app
