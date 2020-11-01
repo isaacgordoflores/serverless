07748b81093bc0813d6b507b6c83c2b05ecfcf30
Isaac Gordo Flores <73773793+isaacgordoflores@users.noreply.github.com>
2020-11-01 18:15:55 +0100

    Añado dos rutas

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')   
 const cors = require('cors')
+const meals = require('./routes/meals')
+const orders = require('./routes/orders')
 
 const app = express()
 /**
@@ -17,9 +19,7 @@ app.use(cors())
  */
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-//Cuando llame a cualquier ruta con get, devuelvo string
-app.get('*',(req, res) => {
-    res.send({ mensaje: 'RESPUESTA PRUEBA' });
-})
+app.use('/api/meals', meals)
+app.use('/api/orders', orders)
 
 module.exports = app
