5a3d553e02cf808e6a9d57bf5358a78f145c9219
Isaac Gordo Flores <73773793+isaacgordoflores@users.noreply.github.com>
2020-11-08 11:34:00 +0100

    Comentarios nuevos

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express') // require para importar dentro de node
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')   
 const cors = require('cors')
@@ -8,16 +8,23 @@ const orders = require('./routes/orders')
 const app = express()
 /**
  * Para configurar buscar en la documentación de cada una
+ * use() es un metodo que permite agregar plugins al servidor express para darle funcionalidades
  */
-app.use(bodyParser.json())
+app.use(bodyParser.json()) 
 app.use(cors())
 
 /**
  * BDD CONN
  * Conectar a traves de las variables de entorno (MONGO_URI) y le paso dos opciones de configuración 
- * 
+ * process.env me permite acceder a las variables de entorno como MONGO_URI y sus variables de configuracion
  */
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+/** '*' cuando llamemos a cualquier ruta con GET devuelve la respuesta de prueba
+app.get('*', (req, res) => {
+    res.send('PRUEBA DE RESPUESTA')
+})
+*/
 
 app.use('/api/meals', meals)
 app.use('/api/orders', orders)
